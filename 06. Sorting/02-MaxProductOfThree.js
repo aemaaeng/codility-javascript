0dc60e14cbe86155055fbe9c2ee42b013d75ee7e
Hyemin Kim <78579776+aemaaeng@users.noreply.github.com>
2022-10-07 17:32:35 +0900

    Modify: 주석 수정

--- 06. Sorting/02-MaxProductOfThree.js
+++ 06. Sorting/02-MaxProductOfThree.js
@@ -1,11 +1,11 @@
 function solution(A) {
   // sort
-  // find the maximum value
-  // R = maximum, Q = maximum - 1, R = maximum - 2
+  let sorted = A.sort((a, b) => a - b);
+  
+  // index: R = end, Q = end - 1, R = end - 2
   let R = A.length - 1;
   let Q = A.length - 2;
   let P = A.length - 3;
-  let sorted = A.sort((a, b) => a - b);
 
   // (음수 x 음수)도 고려
   // 둘 중에 더 큰 거 내보내기
