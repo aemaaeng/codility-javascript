1019cdc473e1138310fd930c67629639aaaa44b0
aemaaeng <haemin120@naver.com>
2022-10-16 16:58:42 +0900

    Refactor: use a for loop instead of .reduce()

--- 02. Arrays/02-OddOccurencesInArray.js
+++ 02. Arrays/02-OddOccurencesInArray.js
@@ -1,19 +1,28 @@
 function solution(A) {
-  // counter 객체를 만든다. {1: 등장 횟수, 2: 등장 횟수 등등}
-  let counter = A.reduce(function (obj, cur) {
-    // if else문 대신 삼항연산자 사용 가능
-    if (obj[cur] === undefined) {
-      obj[cur] = 1;
-    } else {
-      obj[cur] = obj[cur] + 1;
-    }
-    return obj;
-  }, {});
+  // 각 요소를 카운트할 객체를 만든다.
+  let countObj = {};
 
-  // Object.values()에서 홀수인 것의 key 리턴
-  for (key in counter) {
-    if (counter[key] % 2 === 1) {
-      return Number(key);
+  // 반복문을 돌며 요소의 개수를 카운트한다.
+  for (let i = 0; i < A.length; i++) {
+    if (countObj[A[i]] === undefined) {
+      countObj[A[i]] = 0;
     }
+    countObj[A[i]] += 1;
+  }
+
+  // Object.keys로 각 요소를 돌면서 그 갯수가 홀수이면 해당 key를 리턴한다.
+  // let keys = Object.keys(countObj);
+  // for (let i = 0; i < keys.length; i++) {
+  //   if (countObj[keys[i]] % 2 === 1) {
+  //     return Number(keys[i]);
+  //   }
+  // }
+
+  // --> for in 반복문이 더 직관적임
+  for (key in countObj) {
+    if (countObj[key] % 2 === 1) return Number(key);
   }
 }
+
+// test case
+console.log(solution([9, 3, 9, 3, 9, 7, 9]));
