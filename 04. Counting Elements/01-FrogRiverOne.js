7092f1c349f2df15550a0fa811efc157d79bafdb
aemaaeng <haemin120@naver.com>
2022-10-17 20:12:40 +0900

    Test: add test cases

--- 04. Counting Elements/01-FrogRiverOne.js
+++ 04. Counting Elements/01-FrogRiverOne.js
@@ -1,15 +1,18 @@
 function solution(X, A) {
-  // 중복된 요소가 자동으로 제거되도록 Set 인스턴스 생성
   let leaves = new Set();
 
-  // A 배열에서 반복문을 돌기
+  // A를 기준으로 반복문을 돌며 계속 Set에 요소 추가
+  // set의 사이즈가 X와 같아지면
   for (let i = 0; i < A.length; i++) {
-    let cur = A[i]; // 현재 나뭇잎의 position
-
-    leaves.add(cur);
-    // 길이가 같아지면 인덱스 리턴
-    if (leaves.size === X) return i;
+    leaves.add(A[i]);
+    if (leaves.size === X) {
+      return i;
+    }
   }
-  // 없으면 -1
   return -1;
 }
+
+// test cases
+console.log(solution(5, [1, 3, 1, 4, 2, 3, 5, 4])); // expected: 6
+console.log(solution(1, [1])); // expected: 0
+console.log(solution(6, [3, 4, 2, 1, 1, 5, 3])); // expected: -1
