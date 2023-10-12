94c9fe464a85ac0f1ee336b967175ee31b297f4b
aemaaeng <haemin120@naver.com>
2023-10-12 15:29:04 +0900

    :memo: O(logN) solution 추가

--- 10. Prime and composite numbers/00-Exercise.js
+++ 10. Prime and composite numbers/00-Exercise.js
@@ -16,3 +16,22 @@ function solution(N) {
 }
 
 console.log(solution(10)); // expected: 3
+
+// O(logN) solution
+// 각 동전은 자신의 나머지의 개수만큼 뒤집어진다는 점을 이용
+// 나머지의 개수가 홀수인 수는 tail을 보인다.
+// 어떤 수의 제곱이 되는 수는 무조건 나머지의 개수가 홀수이다.
+// 1부터 N까지의 수 중에서 어떤 수의 제곱이 되는 자연수의 개수를 구한다.
+function countOddDivisors(n) {
+  let result = 0;
+  let i = 1;
+
+  while (i * i <= n) {
+    result += 1;
+    i += 1;
+  }
+
+  return result;
+}
+
+console.log(countOddDivisors(10)); // expected: 3
